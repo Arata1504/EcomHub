50941e9bbbf7778237f650bbd27fcbab9a7acfab
MinhTan <tranminhtan2003@gmail.com>
2026-06-02 23:35:06 +0700

    Cập nhật lại code OTP lần 1

--- core/settings.py
+++ core/settings.py
@@ -201,5 +201,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tantran2415@gmail.com'
-EMAIL_HOST_PASSWORD = 'xxxx_xxxx_xxxx_xxxx'  # Mật khẩu ứng dụng (App Password) 16 ký tự từ Google Account
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
